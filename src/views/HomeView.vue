433edfbb4bca903afbce3c7052ea62760a185c70
gexiaolei <gexiaolei@haowushidai.com>
2018-06-05 19:06:55 +0800

    douban project

---
+++ src/views/HomeView.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="home-view has-header">
+  	<sub-nav></sub-nav>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+  import SubNav from '@/components/SubNav'
+  export default {
+  	components: {
+  	  SubNav
+  	}
+  }
+</script>
---
+++ src/views/PagesView.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="pages-view">
+  	<header-bar></header-bar>
+  	<router-view></router-view>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+  import HeaderBar from '@/components/HeaderBar'
+  export default {
+  	name: 'PageView',
+  	components: {
+  	  HeaderBar
+  	}
+  }
+</script>
